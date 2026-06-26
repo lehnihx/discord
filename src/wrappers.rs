ca23a1d58e9ed7719a802b86450676e05026e9d1
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-06-26 18:20:46 +0100

    style(wrappers): format reply_to_command signature multiline

--- src/wrappers.rs
+++ src/wrappers.rs
@@ -1,6 +1,10 @@
 use crate::types::{Context, Error};
 
-pub async fn reply_to_command(ctx: Context<'_>, content: &str, ephemeral: bool) -> Result<(), Error> {
+pub async fn reply_to_command(
+    ctx: Context<'_>,
+    content: &str,
+    ephemeral: bool,
+) -> Result<(), Error> {
     ctx.send(
         poise::CreateReply::default()
             .content(content)
