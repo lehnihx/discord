aaf0808d4a59d2ae7ae90d7a4aaff3a00811a85d
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-06-27 15:06:27 +0100

    feat(constants): switch AI model to openai/gpt-oss-20b and refine AI prompt

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,7 @@
 pub const CUSTOMERS: &[u64] = &[1244750233582440488];
 pub const AI_SPACES_CATEGORY_ID: u64 = 1520135895381053701;
-pub const AI_MODEL: &str = "llama-3.1-8b-instant";
+pub const AI_MODEL: &str = "openai/gpt-oss-20b";
 pub const AI_TEMPERATURE: f32 = 0.7;
 pub const AI_COUNTRY: &str = "SA";
 pub const AI_DOMAINS: &[&str] = &[""];
-pub const AI_PROMPT: &str =
-  "You are a helpful AI assistant inside a Discord forum thread. Reply clearly and concisely, even if he ask in arabic keep it with his same language. ";
+pub const AI_PROMPT: &str = "You are a helpful AI assistant inside a Discord forum thread. Reply clearly and concisely. If asked in arabic keep it with the same language and exact same dialect (eg. ar-SA, ar-...... and same energy received from the user(funny, unserious, professional, ....).";
